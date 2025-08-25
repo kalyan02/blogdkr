4862a1494927c6c2b3f320e5f0fc985955c6bce8
Kalyan <nvsnkalyan@gmail.com>
2025-08-25 08:24:31 +0000

    removed rust impl

--- blogsync/migrations/2025-08-18-084714_add_col/down.sql
+++
@@ -1,3 +0,0 @@
--- This file should undo anything in `up.sql`
-ALTER TABLE `user` DROP COLUMN `refresh_token`;
-
--- blogsync/migrations/2025-08-18-084714_add_col/up.sql
+++
@@ -1,3 +0,0 @@
--- Your SQL goes here
-ALTER TABLE `user` ADD COLUMN `refresh_token` TEXT;
-
